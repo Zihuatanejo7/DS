970967076661a083aa4431f57a05ff6149699d1c
Zihuatanejo7 <zhaozhiyang1997@gmail.com>
2021-09-05 08:35:55 +0800

    NodeList

---
+++ NodeList/NodeList.h
@@ -0,0 +1,42 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+# define OK 1
+# define ERROR 0
+# define TRUE 1
+# define FALSE 0
+
+typedef int Status;
+typedef int ElemType;
+
+typedef struct Node
+{
+	ElemType data;
+	struct Node* next;
+
+}Node;
+
+typedef struct Node* LinkList;
+
+Status Display(LinkList L);
+
+Status InitList(LinkList* L);
+
+int ListLength(LinkList L);
+
+Status ListEmpty(LinkList L);
+
+Status ClearList(LinkList* L);
+
+Status GetElem(LinkList L,int i, ElemType *pe);
+
+Status InsertList(LinkList* L, int i, ElemType e);
+
+int LocateElem(LinkList L, ElemType e);
+
+Status ListDelete(LinkList* L, int i, ElemType* pe);
+
+Status CreateListHead(LinkList* L, int n);
+
+Status CreateListTail(LinkList* L, int n);
