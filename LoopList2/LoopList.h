848ffb651335fc17373054091ca49741c8f1f47c
Zihuatanejo7 <zhaozhiyang1997@gmail.com>
2021-09-06 09:30:38 +0800

    LoopList2

---
+++ LoopList2/LoopList.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+# define ERROR 0
+# define OK 1
+
+typedef int ElemType;
+typedef int Status;
+
+typedef struct node
+{
+    ElemType data;
+    struct node* next;
+}node;
+
+typedef struct node* LinkList;
+
+Status InitList(LinkList* pNode);
+
+Status DeleteList(LinkList* pNode, int i);
+
+Status InsertList(LinkList* pNode, int i,ElemType e);
+
+Status SearchList(LinkList pNode, ElemType e);
+
+int ListLength(LinkList pNode);
+
+void Display(LinkList pNode);
---
+++ LoopList2/test.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+# include "LoopList.h"
+
+int main()
+{
+    LinkList pHead = NULL;
+
+    InitList(&pHead);
+    printf("%d\n", ListLength(pHead));
+    Display(pHead);
+
+    int j;
+    for (j = 1; j <= 10; j++)
+    {
+        InsertList(&pHead, j,j);
+    }
+    printf("%d\n", ListLength(pHead));
+    Display(pHead);
+
+    DeleteList(&pHead, 10);
+    printf("%d\n", ListLength(pHead));
+    Display(pHead);
+    
+    return 0;
+}
+
